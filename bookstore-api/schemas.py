218b4553834d20d1a32235dfcbfb6c38a4438292
Paulo-Marcelo-Git <pmc.silva20@gmail.com>
2025-05-07 09:47:57 -0300

    feat(schemas) ajuste datetime

--- bookstore-api/schemas.py
+++ bookstore-api/schemas.py
@@ -1,20 +1,25 @@
 from pydantic import BaseModel
+from datetime import datetime
+
 
 class BookBase(BaseModel):
     title: str
     author: str
     description: str | None = None
 
+
 class BookCreate(BookBase):
     pass
 
+
 class Book(BookBase):
     id: int
-    create_date: str
-    update_date: str
+    create_date: datetime
+    update_date: datetime
 
     class Config:
         from_attributes = True
+
 
 # Nova schema para retorno enxuto
 class BookSummary(BaseModel):
